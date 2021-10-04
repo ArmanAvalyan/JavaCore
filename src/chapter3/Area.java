8f30eba175ac37ee47cba5f8fa8971bbc92078de
Arman Avalyan <avalyanarman@gmail.com>
2021-10-04 18:16:15 +0400

    added some examples

---
+++ src/chapter3/Area.java
@@ -0,0 +1,11 @@
+package chapter3;
+
+public class Area {
+    public static void main(String[] args) {
+        double pi, r, s;
+        pi = 3.1416;
+        r = 10.8;
+        s = pi * r * r;
+        System.out.println("Area of circle is " + s);
+    }
+}
---
+++ src/chapter3/Light.java
@@ -0,0 +1,16 @@
+package chapter3;
+
+public class Light {
+    public static void main(String[] args) {
+        int lightspeed;
+        long days;
+        long seconds;
+        long distance;
+        lightspeed = 186000;
+        days = 1000;
+        seconds = 24 * 60 * 60;
+        distance = lightspeed * seconds;
+        System.out.print("In " + days);
+        System.out.println(" days light will travel about " + distance + " miles.");
+    }
+}
