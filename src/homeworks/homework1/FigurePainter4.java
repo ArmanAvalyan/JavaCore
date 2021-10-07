74ca2a3160ec08eb4ccc69595b79538c4f280de5
Arman Avalyan <avalyanarman@gmail.com>
2021-10-07 20:00:05 +0400

    added homeworks

--- src/homeworks/homework1/FigurePainter4.java
+++ src/homeworks/homework1/FigurePainter4.java
@@ -2,6 +2,24 @@ package homeworks.homework1;
 
 public class FigurePainter4 {
     public static void main(String[] args) {
-
+        char c = '*';
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 0; j < 4 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print(c + " ");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 4 - i; j++) {
+                System.out.print(c + " ");
+            }
+            System.out.println();
+        }
     }
 }
