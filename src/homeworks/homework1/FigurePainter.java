f8d4fb0431ede21470e4d700d0bb4d9979ab646e
Arman Avalyan <avalyanarman@gmail.com>
2021-10-10 19:51:08 +0400

    added homeworks

--- src/homeworks/homework1/FigurePainter.java
+++ src/homeworks/homework1/FigurePainter.java
@@ -2,10 +2,64 @@ package homeworks.homework1;
 
 public class FigurePainter {
     public static void main(String[] args) {
-        char c = '*';
+
+        // first figure
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // second figure
+        for (int i = 0; i < 4; i++) {
+            for (int j = 4; j > i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // third figure
         for (int i = 1; i <= 4; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print(c + " ");
+            for (int j = 4; j > i; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // fourth figure
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 4; j >= i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+            for (int j = 1; j <= i; j++) {
+                System.out.print("  ");
+            }
+        }
+        System.out.println();
+
+        // rhombus
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 4; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 4; j > i; j--) {
+                System.out.print("* ");
             }
             System.out.println();
         }
